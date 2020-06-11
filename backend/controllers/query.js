727ae20e611cd3961c14ee609fa44890891d3adb
dianneloh9 <dianneloh9@gmail.com>
2020-06-11 17:24:00 +0800

    Added query prog language and topics

--- backend/controllers/query.js
+++ backend/controllers/query.js
@@ -1,15 +1,15 @@
 const pool = require('../db');
 
 const getProgLanguages = async (req, res) => {
-    const response = await pool.query("SELECT JSON_AGG(JSON_BUILD_OBJECT(prog_id, prog_name)) FROM prog_languages");
+    const prog_langs = await pool.query("SELECT JSON_AGG(JSON_BUILD_OBJECT(prog_id, prog_name)) FROM prog_languages");
 
-    res.send(response.rows[0]);
+    res.send(prog_langs.rows[0]);
 }
 
 const getTopics = async (req, res) => {
-    const response = await pool.query("SELECT JSON_AGG(JSON_BUILD_OBJECT(topic_id, topic)) FROM topics");
+    const topics = await pool.query("SELECT JSON_AGG(JSON_BUILD_OBJECT(topic_id, topic)) FROM topics");
 
-    res.send(response.rows[0]);
+    res.send(topics.rows[0]);
 }
 
 module.exports = {
