3d20b328a985c42b98d1f2910d64d6bd6f5c5f9a
siddarth2824 <siddarth2824@gmail.com>
2020-06-23 21:13:02 +0800

    Fixed state update of interview timings in booking

--- src/components/bookingForm/DaysColumn.js
+++ src/components/bookingForm/DaysColumn.js
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TimeBlock from './TimeBlock';
 
-const DaysColumn = ({ day, month, date }) => {
+const DaysColumn = (props) => {
     const timeArr = ["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM", "5:00 PM", "7:00 PM", "9:00 PM", "11:00 PM", "1:00 AM"];
-    const[timings, setTimings] = useState([]);
+    const[timings, setTimings] = useState({
+        date: props.dateObj.format('MM/DD/YYYY'),
+        timeSlots: []
+    });
 
     const callbackForTime = (timeValues) => {
-        setTimings(timeValues);
+        setTimings(prevState => {
+           return  {
+            ...prevState, 
+            timeSlots: [...prevState.timeSlots, timeValues]}
+        });
     }
 
-    console.log(timings);
+    useEffect(() => {
+        if(timings.timeSlots.length <= 0){
+            return;
+        }
+        props.onDaysChange(timings);
+    }, [timings])
+
+    //console.log(timings);
 
     return (
         <div className="one wide column"  style={{ paddingLeft:"2px" }}>
             <div className="row">
                 <h3 className="ui center aligned header" style={{ marginRight: '18px' }}>
-                    {day}
+                    {props.dateObj.format('ddd')}
                 </h3>
             </div>
             <div className="row">
                 <h3 className="ui center aligned grey header" style={{ fontWeight: 'lighter', marginRight: '18px'}}>
-                    {month}
+                    {props.dateObj.format('MMM')}
                     &nbsp;
-                    {date}
+                    {props.dateObj.format('Do')}
                 </h3>
             </div>
             {timeArr.map((time) => {
