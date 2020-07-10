6ab2d275d65e452590fd4df935ed4ec942d9318e
dianneloh9 <dianneloh9@gmail.com>
2020-07-10 15:15:47 +0800

    Fixed connection to db

--- backend/db.js
+++ backend/db.js
@@ -2,21 +2,31 @@ const Pool = require('pg').Pool;
 
 const { db_user, db_password, db_host, db_port, db_name } = require('./config');
 
-let pool;
-if (process.env.NODE_ENV === 'production') {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+let config;
+
+if (process.env.DATABASE_URL) {
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
     ssl: true
-  });
+  };
 } else {
-  pool = new Pool({
+  config = {
     user: db_user,
     password: db_password,
     host: db_host,
     port: db_port,
     database: db_name,
-  });
+  }
 }
+
+const pool = new Pool(config);
 
 pool.on('error', (err, client) => {
   console.error('Unexpected error on postgres client', err);
