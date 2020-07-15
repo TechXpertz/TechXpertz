69341d3110a14a29ab8ff6e1bca38a314e557227
siddarth2824 <siddarth2824@gmail.com>
2020-07-15 21:34:59 +0800

    Created user profile component

--- src/components/userprofile/Profile.js
+++ src/components/userprofile/Profile.js
@@ -1,23 +1,30 @@
-import React, { Fragment } from "react";
-import { useAuth0 } from "../../react-auth0-spa";
+import React from 'react';
+import UserNavBar from '../dashboard/UserNavBar';
+import ProfileContent from './ProfileContent';
 
 const Profile = () => {
-  const { loading, user, logout, isAuthenticated } = useAuth0();
-
-  if (loading || !user) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <Fragment>
-      <img src={user.picture} alt="Profile" />
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-      <code>{JSON.stringify(user, null, 2)}</code>
-    </Fragment>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <UserNavBar selected={{ about: false, FAQ: false, profile: true }} />
+      <ProfileContent />
+    </div>
   );
 };
 
 export default Profile;
+
+// console.log(user);
+// if (loading || !user) {
+//   return <div>Loading...</div>;
+// }
+
+// return (
+//   <>
+//     <img src={user.picture} alt='Profile' />
+//     {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+//     <h2>{user.name}</h2>
+//     <p>{user.email}</p>
+//     <code>{JSON.stringify(user, null, 2)}</code>
+//   </>
+// );
